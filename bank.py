5a4790db766a2f5a537abd92bbfca220e903dd7d
SamiklaSSS <149561953+SamiklaSSS@users.noreply.github.com>
2024-01-17 10:11:40 +0200

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,11 +1,11 @@
-# 
 # Darbības ar masīviem - https://www.w3schools.com/python/python_arrays.asp
 # Datu tipi, mekle float - https://www.w3schools.com/python/python_datatypes.asp
 # Pārbaudēm izmanto if...else - https://www.w3schools.com/python/python_conditions.asp
 # Cipari https://www.w3schools.com/python/python_numbers.asp
-# 
+
 
 balance = 0
+
 
 while True:
     print("\nBanking Options:")
@@ -14,19 +14,46 @@ while True:
     print("3. Check Balance")
     print("4. Exit")
 
+
     choice = input("Enter your choice (1-4): ")
 
+
+  
     if choice == '1':
-        # papildini kodu šeit
-        pass
-    elif choice == '2':
-        # papildini kodu šeit
-        pass
+        deposit = (float(input("Enter the amount to deposit:")))
+        if deposit > 0:
+          balance += deposit
+          print("Deposit successful. New balance:", balance)
+        else:
+          print("Error, cannot deposit.") 
+
+
+  
+  
+    elif choice == '2' :
+      withdraw = (float(input("Enter the amount to withdraw:")))
+      if withdraw > balance:
+          print("Insufficient balance. Cannot withdraw.")
+      elif withdraw < 0:
+          print("Error, cannot withdraw")
+      else:
+          balance -= withdraw
+          print("Withdraw successful. New balance:", balance)
+          print(balance)
+          pass
+
+
+  
+  
     elif choice == '3':
-        # papildini kodu šeit
+        print("Current balance:", balance)
         pass
+
+
+  
+  
     elif choice == '4':
         print("Exiting the banking system. Thank you!")
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 5.")
+        print("Invalid choice. Please enter a number between 1 and 4.")
